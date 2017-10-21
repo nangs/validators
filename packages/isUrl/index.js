d4bfd3f9cd01f4b361b7ab2a88b4b7298bbbd056
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-10-21 01:48:04 -0400

    isUrl

--- packages/isUrl/index.js
+++ packages/isUrl/index.js
@@ -1,4 +1,7 @@
-export default str => {
-    const pattern = /\b(https?|ftp|file):\/\/[\-A-Za-z0-9+&@#\/%?=~_|!:,.;]*[\-a-zA-Z‌0-9+&@#\/%=~_|]*/;
-    return pattern.test(str);
+export default (str, options={
+    protocols: ['https', 'http']
+}) => {
+    const protocols = options.protocols.join('|');
+    const pattern = `\\b(${protocols}):\\/\\/[\\-A-Za-z0-9+&@#\\/%?=~_|!:,.;]*[\\-a-zA-Z‌0-9+&@#\\/%=~_|]*`;
+    return new RegExp(pattern).test(str);
 }
--- packages/isUrl/index.test.js
+++ packages/isUrl/index.test.js
@@ -5,4 +5,6 @@ test('valid url', t => t.true(sut('https://www.quillio.io')));
 
 test('invalid protocol', t => t.false(sut('badprotocol://quillio.io')));
 
+test('custom protocol', t => t.true(sut('ftp://quillio.io', { protocols: ['ftp'] })));
+
 test('missing protocol', t => t.false(sut('quillio.io')));
